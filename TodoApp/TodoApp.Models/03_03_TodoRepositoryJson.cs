a4250c54abd06c749e83176ec4dd31c35794c6c3
krjka <kajangzzang@gmail.com>
2021-01-26 15:53:54 +0900

    JSON 파일 입출력 테스트

---
+++ TodoApp/TodoApp.Models/03_03_TodoRepositoryJson.cs
@@ -0,0 +1,48 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace TodoApp.Models
+{
+    public class TodoRepositoryJson : ITodoRepository
+    {
+        private readonly string _filePath;
+        private static List<Todo> _todos = new List<Todo>();
+
+        //public TodoRepositoryFile()
+        //{
+        //    _todos = new List<Todo>
+        //    {
+        //        new Todo { Id = 1, Title = "ASP.NET Core 학습", IsDone = false },
+        //        new Todo { Id = 2, Title = "Blazor 학습", IsDone = false },
+        //        new Todo { Id = 3, Title = "C# 학습", IsDone = true },
+        //    };
+        //}
+
+        public TodoRepositoryJson(string filePath = @"G:\Study\Todos.json")
+        {
+            this._filePath = filePath;
+            var todos = File.ReadAllText(filePath, Encoding.Default);
+            //Json 패키지 이용하여 읽기
+            _todos = JsonConvert.DeserializeObject<List<Todo>>(todos);
+        }
+
+        // 인-메모리 데이터베이스 사용 영역
+        public void Add(Todo model)
+        {
+            model.Id = _todos.Max(t => t.Id) + 1;
+            _todos.Add(model);
+
+            //Json 파일 저장
+            string json = JsonConvert.SerializeObject(_todos, Formatting.Indented);
+            File.WriteAllText(_filePath, json);
+        }
+
+        public List<Todo> GetAll()
+        {
+            return _todos;
+        }
+    }
+}
---
+++ TodoApp/TodoApp.WindowsFormsApp/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Windows.Forms;
+using TodoApp.Models;
+
+namespace TodoApp.WindowsFormsApp
+{
+    public partial class Form1 : Form
+    {
+        private readonly TodoRepositoryJson _repository;
+
+        public Form1()
+        {
+            InitializeComponent();
+            _repository = new TodoRepositoryJson(@"G:\Study\Todos.json");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DisplayData();
+        }
+
+        private void DisplayData()
+        {
+            this.dataGridView1.DataSource = _repository.GetAll();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            string title = txtTitle.Text;
+            bool isDone = blnIsDone.Checked;
+
+            Todo todo = new Todo { Title = title, IsDone = isDone };
+            _repository.Add(todo);
+
+            DisplayData();
+        }
+    }
+}
